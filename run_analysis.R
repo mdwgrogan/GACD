f04b148026e24e1cc48e1f45f4e5bdcdc4f4d2de
Michael Grogan <mdwgrogan@gmail.com>
2016-04-02 23:19:31 -0400

    unzip the data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,10 @@
-dsUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip";
-dsName <- "data/UCI_HAR_Dataset.zip";
+dsUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dsName <- "data/UCI_HAR_Dataset.zip"
 
 # check if we've already downloaded the data, and download if we haven't
-if (!(dir.exists('data'))) {dir.create('data')};
-if (!(file.exists(dsName))) {download.file(dsUrl, dsName)};
+if (!(dir.exists('data'))) {dir.create('data')}
+if (!(file.exists(dsName))) {download.file(dsUrl, dsName)}
 
+# unzip the archive
+unzip(dsName, exdir = 'data')
+unzipped <- unzip(dsName, list=T)
