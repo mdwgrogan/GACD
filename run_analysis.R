97befe5ef685523a85276ce9ed7a4793b74f968c
Michael Grogan <mdwgrogan@gmail.com>
2016-04-03 21:29:15 -0400

    use a function to bind the three relevant parts of the dataset together

--- run_analysis.R
+++ run_analysis.R
@@ -7,4 +7,30 @@ if (!(file.exists(dsName))) {download.file(dsUrl, dsName)}
 
 # unzip the archive
 unzip(dsName, exdir = 'data')
-unzipped <- unzip(dsName, list=T)
+
+# get the unaliased activity descriptions
+unalias <- fread('data/UCI HAR Dataset/activity_labels.txt', col.names = c("alias", "activity"), header=F)
+feature_names <- fread('data/UCI HAR Dataset/features.txt', col.names = c("alias", "featureName"), header=F)[, featureName]
+
+bound_datasets <- function (data_dir) {
+  target <- paste('data/UCI HAR Dataset/', data_dir, sep = "");
+  
+  # load the full vector (X) data set into a data frame
+  X <- paste(target, '/X_', data_dir, '.txt', sep = "");
+  dtBase <- fread(X, col.names = feature_names, header = F);
+  
+  # load the subject data into a data frame
+  subj <- paste(target, '/subject_', data_dir, '.txt', sep = "");
+  dtSubj <- fread(subj, col.names = "subj_id");
+  
+  # load the activity (Y) data set
+  Y <- paste(target, '/y_', data_dir, '.txt', sep = "");
+  dtAlias <- merge(fread(Y, col.names = "alias"), unalias, by = "alias");
+  
+  # bind the rows together
+  dtDir <- cbind(dtSubj, dtAlias, dtBase);
+  return(data.table(dtDir))
+}
+
+dtTrain <- bound_datasets('train')
+dtTest <- bound_datasets('test')
